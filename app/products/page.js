fde80352f795c4730bd61c46b595d62eea45ab32
dave <robel_h@yahoo.com>
2026-03-02 21:19:29 +0000

    Fix

--- app/products/page.js
+++ app/products/page.js
@@ -5,20 +5,20 @@ import ProductsClient from "./ProductsClient";
 import { NextResponse } from "next/server";
 const { Redis } = require("@upstash/redis");
 
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL  ,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN ,
-});
+
+console.log(process.env.UPSTASH_REDIS_REST_URL);
+console.log(process.env.UPSTASH_REDIS_REST_TOKEN);
+
 
 export default async function ProductsPage() {
   let products = null;
   try {
-    const cached = await redis.get("products");
+    const cached = null //await redis.get("products");
 
     if (cached) {
       console.log("Serving from Redis cache");
       //return res.json(cached);
-      products = cached;
+      //products = cached;
     }
  
     return (
