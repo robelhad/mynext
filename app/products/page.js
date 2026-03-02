a4aef8a0742cdd5464de5fe1d91e3318d1bb1f29
dave <robel_h@yahoo.com>
2026-03-02 20:52:51 +0000

    Fix

--- app/products/page.js
+++ app/products/page.js
@@ -1,4 +1,14 @@
+import { prisma } from "@/lib/prisma";
+//import ProductCard from "./ProductCard";
+import Link from "next/link";
+import ProductsClient from "./ProductsClient";
+import { NextResponse } from "next/server";
+const { Redis } = require("@upstash/redis");
 
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL  ,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN ,
+});
 
 export default async function ProductsPage() {
 
