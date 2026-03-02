63574abeec521b7d87dd97fff79f7a795ed28238
dave <robel_h@yahoo.com>
2026-03-02 22:42:16 +0000

    Fix

--- app/products/page.js
+++ app/products/page.js
@@ -2,10 +2,10 @@
 export default async function ProductsPage() {
   
 
-  console.log(process.env.UPSTASH_REDIS_REST_URL);
-  console.log(process.env.UPSTASH_REDIS_REST_TOKEN);
+  console.log(process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_URL);
+  console.log(process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_TOKEN);
     return (
-  <div>Success loading products {process.env.UPSTASH_REDIS_REST_URL}</div>)
+  <div>Success loading products {process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_URL}  {process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_TOKEN}</div>)
  
   
 }
