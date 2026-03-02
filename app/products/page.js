59308f6115316eb15699896d8a494e1a56b225ad
dave <robel_h@yahoo.com>
2026-03-02 21:34:39 +0000

    Fix

--- app/products/page.js
+++ app/products/page.js
@@ -1,33 +1,8 @@
-import { prisma } from "@/lib/prisma";
-//import ProductCard from "./ProductCard";
-import Link from "next/link";
-import ProductsClient from "./ProductsClient";
-import { NextResponse } from "next/server";
-const { Redis } = require("@upstash/redis");
-
-
-console.log(process.env.UPSTASH_REDIS_REST_URL);
-console.log(process.env.UPSTASH_REDIS_REST_TOKEN);
-
 
 export default async function ProductsPage() {
-  let products = null;
-  try {
-    const cached = null //await redis.get("products");
-
-    if (cached) {
-      console.log("Serving from Redis cache");
-      //return res.json(cached);
-      //products = cached;
-    }
- 
+  
     return (
-  <div>Success loading products</div>
-    )
- } catch (error) {
-      console.error("Error fetching products:", error);
-      return <div>Error loading products</div>;
-    }
-
+  <div>Success loading products</div>)
+ 
   
 }
